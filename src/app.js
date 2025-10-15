d6d1c337c0b3a520a9d4334cbef1b69a83724bd8
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2025-10-15 22:55:10 +0000

    Agregar soporte para versiones de Node.js y mejorar la generación de dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['puedes', 'jogger', 'radio']; 
+let extensions = ["es", "com", "net", "us", "io"]
+
+function hack(noun, extension) {
+	let longitud_de_extension = extension.length;
+	if (noun.length >= longitud_de_extension) {
+		return noun.slice(-longitud_de_extension) === extension
+	}
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let content = ''
+	pronoun.forEach(
+		pronombre => {
+			adj.forEach(
+				adjetivo => {
+					noun.forEach(
+						nombre =>{
+							extensions.forEach(
+								extension => {
+									if (hack(nombre,extension)) content+=`<li class="list-group">${pronombre}${adjetivo}${nombre.slice(0,nombre.length- extension.length)}.${extension}</li>`
+									else content+= `<li class="list-group">${pronombre}${adjetivo}${nombre}.${extension}</li>`
+								}
+							)
+						}
+					)
+				}
+			)
+		}
+	)
+  
+  document.querySelector("#dominios").innerHTML= content
 };
