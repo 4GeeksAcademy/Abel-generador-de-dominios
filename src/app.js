dde964f1837f63b782038e369e4bb51d41b115b5
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2025-10-17 18:13:42 +0000

    formating code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['puedes', 'jogger', 'radio']; 
-let extensions = ["es", "com", "net", "us", "io"]
+
+
 
 function hack(noun, extension) {
 	let longitud_de_extension = extension.length;
@@ -17,27 +13,35 @@ function hack(noun, extension) {
 	}
 }
 
-window.onload = function() {
-  //write your code here
-  let content = ''
-	pronoun.forEach(
-		pronombre => {
-			adj.forEach(
-				adjetivo => {
-					noun.forEach(
-						nombre =>{
-							extensions.forEach(
-								extension => {
-									if (hack(nombre,extension)) content+=`<li class="list-group">${pronombre}${adjetivo}${nombre.slice(0,nombre.length- extension.length)}.${extension}</li>`
-									else content+= `<li class="list-group">${pronombre}${adjetivo}${nombre}.${extension}</li>`
-								}
-							)
-						}
-					)
-				}
-			)
-		}
-	)
-  
-  document.querySelector("#dominios").innerHTML= content
+
+window.onload = function () {
+	//write your code here
+	let content = ''
+	let pronoun = ['the', 'our'];
+	let adjective = ['great', 'big'];
+	let noun = ['puedes', 'jogger', 'radio'];
+	let extensions = ["es", "com", "net", "us", "io"]
+
+	pronoun.forEach( pronombre => {
+		adjective.forEach( adjetivo => {
+			noun.forEach( nombre => {
+				extensions.forEach( extension => {
+					if (hack(nombre, extension)) {
+						content += `
+							<li class="list-group">
+								${pronombre}${adjetivo}${nombre.slice(0, nombre.length - extension.length)}.${extension}
+							</li>`
+					}
+					else {
+						content += `
+							<li class="list-group">
+								${pronombre}${adjetivo}${nombre}.${extension}
+							</li>`
+					}
+				})
+			})
+		})
+	})
+
+	document.querySelector("#dominios").innerHTML = content
 };
